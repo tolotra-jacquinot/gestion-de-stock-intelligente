35accddffdbf803a73b4a61c3049ff1fb08defda
Tolotra Jacquinot <tolotrajacquinot@gmail.com>
2026-08-07 13:40:29 +0300

    Implement Product API

--- hospital-backend/backend/urls.py
+++ hospital-backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path(
-        "api/",
-        include("accounts.urls")
-    ),
+    # Authentification
+    path("api/auth/", include("accounts.urls")),
+
+    # Produits
+    path("api/", include("products.urls")),
 ]
