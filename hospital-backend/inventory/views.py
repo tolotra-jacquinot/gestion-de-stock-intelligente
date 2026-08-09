0e3805d90576dede345863d9e2ab8afd703b4716
Tolotra Jacquinot <tolotrajacquinot@gmail.com>
2026-08-09 21:31:20 +0300

    Protect stock movement history

--- hospital-backend/inventory/views.py
+++ hospital-backend/inventory/views.py
@@ -9,6 +9,8 @@ class StockMovementViewSet(viewsets.ModelViewSet):
     queryset = StockMovement.objects.all().order_by("-created_at")
     serializer_class = StockMovementSerializer
     permission_classes = [StockMovementPermission]
+    
+    http_method_names = ["get", "post", "head", "options"]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
