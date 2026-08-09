b06ebe6fbdf39e557cda4b03b5000bbce67a863c
Tolotra Jacquinot <tolotrajacquinot@gmail.com>
2026-08-09 21:53:43 +0300

    Add stock alert API

--- hospital-backend/products/urls.py
+++ hospital-backend/products/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+
+from .views import ProductViewSet, stock_alerts
+
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("alerts/", stock_alerts, name="stock-alerts"),
+]
+
+urlpatterns += router.urls
