d287c5b3665d336320a8749c2e0ec6efda0c7b72
Tolotra Jacquinot <tolotrajacquinot@gmail.com>
2026-08-09 13:15:42 +0300

    Implement StockMovement API

--- hospital-backend/inventory/views.py
+++ hospital-backend/inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import StockMovement
+from .serializers import StockMovementSerializer
+
+
+class StockMovementViewSet(viewsets.ModelViewSet):
+    queryset = StockMovement.objects.all().order_by("-created_at")
+    serializer_class = StockMovementSerializer
+    permission_classes = [IsAuthenticated]
